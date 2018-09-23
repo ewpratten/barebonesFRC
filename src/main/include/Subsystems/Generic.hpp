55f1a1fac6121ff5eaace1e852b4aed4b1324ebc
Evan Pratten <ewpratten@gmail.com>
2018-09-23 16:46:59 -0400

    Documentation for generic subsystem

--- src/main/include/Subsystems/Generic.hpp
+++ src/main/include/Subsystems/Generic.hpp
@@ -1,3 +1,5 @@
+//! Generic controls for motors
+
 #ifndef _GENERIC_HG_
 #define _GENERIC_HG_
 
@@ -9,15 +11,19 @@
 
 class Generic: public frc::Subsystem {
 public:
-	Generic();
-	~Generic();
+	Generic();	//!< Class constructor
+	~Generic(); //!< Class destructor
 
-//	void InitDefaultCommand(void) override;
+	/*! \fn setSpeed(int ID, double speed)
+			\breif Set the raw speed of a motor
+			\param ID The id of the motor to manipulate
+			\param speed The speed to set the motor too
+	*/
 	void setSpeed(int ID, double speed);
 
 private:
-	can::WPI_TalonSRX* pMotor1;
-	can::WPI_TalonSRX* pMotor2;
+	can::WPI_TalonSRX* pMotor1;	//!< Pointer to a Talon
+	can::WPI_TalonSRX* pMotor2;	//!< Pointer to a Talon
 
 };
 

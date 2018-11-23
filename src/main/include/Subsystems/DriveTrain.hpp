548b8d7376fc999ade65cad42ba7802c5a351fff
Evan Pratten <ewpratten@gmail.com>
2018-11-23 10:26:18 -0500

    Revert to fix include errors

--- src/main/include/Subsystems/DriveTrain.hpp
+++ src/main/include/Subsystems/DriveTrain.hpp
@@ -20,7 +20,14 @@ public:
 
 	void InitAutonomousMode(void);
 	void InitDefaultCommand(void) override;
-	
+	void InitMotionProfilingMode(void);
+
+	// Used for autonomous mode
+	void DriveSetup(void);
+	void Drive(double distance, double speed);
+	void TurnSetup(void);
+	void Turn(void);
+
 	// These are the same as the DifferentialDrive class:
 	void ArcadeDrive(double xSpeed, double zRotation);
 	void CurvatureDrive(double xSpeed, double zRotation, bool isQuickTurn);
@@ -28,8 +35,38 @@ public:
 
 	double GetAngle(void);
 
+	can::WPI_TalonSRX* GetLeftFrontMotor();  // Needed by Motion Profiler
+	int GetLeftClosedLoopError(void);
+	double GetLeftDistance(void);
+	double GetLeftPosition(void);
+	int GetLeftVelocity(void);
+
+	can::WPI_TalonSRX* GetRightFrontMotor(); // Needed by Motion Profiler
+	int GetRightClosedLoopError(void);
+	double GetRightDistance(void);
+	double GetRightPosition(void);
+	int GetRightVelocity(void);
+
+	double GetRotateToAngleRate(void);
+	double GetTargetPosition(void);
+
 	bool IsDriving(void);
 	bool IsTurning(void);
+
+	void ResetDrive(void);
+	void ResetEncoders(void);
+	void ResetGyro(void);
+
+	void SetEncoders(void);
+	void SetRotateToAngleRate(double rate);
+	void SetSetpoint(double setpoint);
+	void SetTargetPosition(double target);
+
+	// Send motor data to SmartDashboard
+	void Trace(void);
+	void Trace(WPI_TalonSRX* pTalonSRX, const std::string name);
+
+	virtual void PIDWrite(double output) override;
 
 private:
 	can::WPI_TalonSRX* pLeftFrontMotor;
@@ -40,6 +77,10 @@ private:
 	frc::DifferentialDrive* pRobotDrive;
 
 	AHRS* pGyro;
+	PIDController* pTurnController;
+
+	double dTargetPostionRotations = 0.00;
+	double dRotateToAngleRate;
 };
 
 #endif
